889b39d7f6775ebf53fa6f77f184c523bcab314a
lukewire129 <lukewire129@gmail.com>
2024-10-22 10:13:09 +0900

    Avalonia Sample url 변경

--- src/pages/AvaloniaPage.jsx
+++ src/pages/AvaloniaPage.jsx
@@ -2,7 +2,7 @@ function AvaloniaPage() {
   return (
     <div>
       <iframe
-        src="https://lukewire129.github.io/avalonia"
+        src="https://delightful-profiterole-0fa25d.netlify.app/"
         frameborder="0"
         style={{ width: "100vw", height: "100vh" }}
       ></iframe>
