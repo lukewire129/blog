a787bac2d2eb244a877a7e469b36ffb6f3a18f7d
lukewire129 <lukewire129@gmail.com>
2024-10-23 15:42:35 +0900

    화면 업데이트 진행 중...

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import { socket } from "./utils/socket";
 import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 const MainPage = React.lazy(() => import("./pages/MainPage"));
 const BlogPage = React.lazy(() => import("./pages/BlogPage"));
-const AvaloniaPage = React.lazy(() => import("./pages/AvaloniaPage"));
 const BlogViewerPage = React.lazy(() => import("./pages/BlogViewerPage"));
-
-const Header = styled.div`
-  background-color: #f0f4f8;
-  color: white;
-  padding: 10px;
-  text-align: center;
-  width: "100vw";
-`;
-
-const NaVul = styled.ul`
-  list-style-type: none;
-  padding: 0;
-`;
-const Navulli = styled.li`
-  display: inline;
-  margin: 0 10px;
-`;
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
 const Nava = styled(Link)`
   color: #007acc;
   font-size: 24px;
@@ -31,46 +21,61 @@ const Nava = styled(Link)`
 `;
 
 function App() {
+  const [recentPost, setRecentPost] = useState([]);
+  const [tags, setTags] = useState([]);
+  const tagList = Array.isArray(tags) ? tags : [];
+  useEffect(() => {
+    socket.get("/recentblogs.json").then((response) => {
+      const { code, data } = response;
+      setRecentPost(
+        data.files
+          .sort((a, b) => new Date(b.metadata.date) - new Date(a.metadata.date))
+          .slice(0, 3)
+      );
+    });
+    socket.get("/tagBlogs.json").then((response) => {
+      const { code, data } = response;
+      console.log(data);
+      setTags(data);
+    });
+  }, []);
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div style={{ backgroundColor: "#fafafa" }}>
-        <Header>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
+        <div className="header">
+          <div class="logo" style={{ marginBottom: "10px" }}>
             <strong style={{ color: "black", fontSize: "24px" }}>
               lukewire` Blog
             </strong>
-            <nav>
-              <ul>
-                <li>
-                  <Nava to="/">홈</Nava>
-                </li>
-                <li>
-                  <Nava to="/post">최근 포스트</Nava>
-                </li>
-                <li>
-                  <Nava to="/">카테고리</Nava>
-                </li>
-                <li>
-                  <Nava to="/">소개</Nava>
-                </li>
-              </ul>
-            </nav>
           </div>
-        </Header>
-        <div>
-          <Suspense fallback={<div>Loading....</div>}>
-            <Routes>
-              <Route exact path="/" element={<MainPage />}></Route>
-              <Route path="/avalonia" element={<AvaloniaPage />}></Route>
-              <Route path="/post" element={<BlogPage />}></Route>
-              <Route path="/postdetail" element={<BlogViewerPage />}></Route>
-            </Routes>
-          </Suspense>
+          <div className="nav">
+            <ul style={{ listStyle: "none" }}>
+              <li>
+                <Nava to="/">Home</Nava>
+              </li>
+              <li>
+                <Nava to="/post">Post</Nava>
+              </li>
+              <li>
+                <Nava to="/">Catecory</Nava>
+              </li>
+              <li>
+                <Nava to="/">About</Nava>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 20%" }}>
+          <div>
+            <Suspense fallback={<div>Loading....</div>}>
+              <Routes>
+                <Route exact path="/" element={<MainPage />}></Route>
+                <Route path="/post" element={<BlogPage />}></Route>
+                <Route path="/postdetail" element={<BlogViewerPage />}></Route>
+              </Routes>
+            </Suspense>
+          </div>
+          <Sidebar tags={tags} />
         </div>
         <footer>
           <p>© 2024 내 개인 블로그. 모든 권리 보유.</p>
